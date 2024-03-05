7a075dc78a19d301180e10ce25ce91902d182654
Lazy Hippopotamus <lazy@hippo.com>
2024-03-05 10:32:53 -0500

    Got it working really well

--- sitix.cpp
+++ sitix.cpp
@@ -1,6 +1,7 @@
 /* Sitix, by Tyler Clarke
     An efficient templating engine written in C++, meant to reduce the pain of web development.
     Sitix templating is optional; files that desire Sitix templating should include [!] as the first three bytes. Otherwise, sitix will simply copy them.
+    Files may also use [?] as the first three bytes; this informs the Sitix engine that the file should not be rendered (most templates will use this).
     Sitix works with a simple scope structure. Variables are set with [=name content] OR [=name-]content[/]. Variables are accessed with [^name].
     Variables can contain other variable accesses! This allows functions of a sort. For instance,
     
@@ -106,6 +107,16 @@
 #include <stdint.h>
 #include <vector>
 #include <stdlib.h>
+#include <ftw.h>
+
+
+#define INFO     "\033[32m[  INFO   ]\033[0m "
+#define ERROR  "\033[1;31m[  ERROR  ]\033[0m "
+#define WARNING  "\033[33m[ WARNING ]\033[0m "
+
+
+const char* outputDir = "output"; // global variables are gross but, eh, screw you
+const char* siteDir = "";
 
 
 char* strdupn(const char* thing, size_t length) { // copy length bytes of a string to a new, NULL-terminated C string
@@ -114,6 +125,54 @@ char* strdupn(const char* thing, size_t length) { // copy length bytes of a stri
     for (size_t i = 0; i < length; i ++) {
         ret[i] = thing[i];
     }
+    return ret;
+}
+
+
+template <typename N>
+N min(N one, N two) {
+    if (one < two) {
+        return one;
+    }
+    return two;
+}
+
+
+int endcmp(const char* one, const char* two) { // check how many bytes are shared at the ends of two strings
+    size_t oneLen = strlen(one);
+    size_t twoLen = strlen(two);
+    int i;
+    for (i = 0; i < min(oneLen, twoLen); i ++) {
+        if (one[oneLen - i - 1] != two[twoLen - i - 1]) {
+            return i;
+        }
+    }
+    return i;
+}
+
+
+char* transmuted(const char* from, const char* to, const char* path) { // transmute a pathname.
+    // calling transmuted("/dev", "/opt", "/dev/null") would return "/opt/null", for instance.
+    if (path[0] == '.') {
+        path = (const char*)(((char*)path) + 1);
+    }
+    size_t fromLen = strlen(from);
+    size_t toLen = strlen(to);
+    size_t pathLen = strlen(path);
+    size_t retLen = pathLen - fromLen + toLen;
+    bool needsSep = false;
+    if ((to[toLen - 1] != '/') && (path[0] != '/')) {
+        needsSep = true;
+        retLen ++;
+    }
+    char* ret = (char*)malloc(retLen + 1);
+    ret[retLen] = 0; // NULL-termination!
+    memcpy(ret, to, toLen);
+    if (needsSep) {
+        ret[toLen] = '/';
+        toLen += 1;
+    }
+    memcpy(ret + toLen, path + fromLen, pathLen - fromLen);
     return ret;
 }
 
@@ -154,6 +213,7 @@ struct Object : Node { // Sitix objects contain a list of *nodes*, which can be 
     // Variable dereferences are considered operations and will not be executed until the relevant section is rendered; it is important to make sure your
     // Sitix code is aware of the scope caveats at all times.
     std::vector<Node*> children;
+    bool isTemplate = false;
 
     Object() {
         isObject = true;
@@ -213,7 +273,7 @@ struct Object : Node { // Sitix objects contain a list of *nodes*, which can be 
         return NULL;
     }
 
-    void replace(Object* obj) { // LEAKS MEMORY!
+    bool replace(Object* obj) { // LEAKS MEMORY BY FAILING TO FREE REPLACED OBJECTS!
         bool didReplace = false;
         for (size_t i = 0; i < children.size(); i ++) {
             if (children[i] -> isObject) {
@@ -227,9 +287,10 @@ struct Object : Node { // Sitix objects contain a list of *nodes*, which can be 
         }
         if (!didReplace) {
             if (parent != NULL) {
-                parent -> replace(obj);
+                didReplace = parent -> replace(obj);
             }
         }
+        return didReplace;
     }
 
     void debugPrint() {
@@ -252,14 +313,24 @@ struct Include : Node { // adds a file (useful for dynamic templating)
     char* fname;
 
     void render(int fd, Object* scope, bool dereference) {
-        int file = open(fname, O_RDONLY);
+        char* transmuteName = transmuted("", siteDir, fname);
+        int file = open(transmuteName, O_RDONLY);
         if (file == -1) {
-            printf("Couldn't open %s for inclusion.\n", fname);
+            printf(ERROR "Couldn't open %s for inclusion.\n", transmuteName);
             return;
         }
         struct stat data;
-        stat(fname, &data);
+        if (stat(transmuteName, &data)) {
+            printf(ERROR "Can't grab file information for %s.\n", transmuteName);
+            perror("\tstat");
+        }
         char* map = (char*)mmap(0, data.st_size, PROT_READ, MAP_SHARED, file, 0);
+        if (map == MAP_FAILED) {
+            printf(ERROR "Can't memory map %s for inclusion in another file! The output \033[1mwill\033[0m be malformed.\n", transmuteName);
+            printf("\tIf %s is a template file, the file it is templating will most likely be left empty.\n", transmuteName);
+            perror("\tmmap");
+            return;
+        }
         Object* object = new Object;
         *object = string2object(map, data.st_size);
         for (Node* n : object -> children) { // INEFFICIENT method of unpacking the loaded file into the parent context.
@@ -283,7 +354,7 @@ struct Dereference : Node { // dereference and render an Object (the [^] operato
             found = scope -> lookup(name);
         }
         if (found == NULL) {
-            printf("Couldn't find %s!\n", name);
+            printf(ERROR "Couldn't find %s! The output \033[1mwill\033[0m be malformed.\n", name);
             return;
         }
         found -> render(fd, scope, true);
@@ -298,10 +369,14 @@ struct Dereference : Node { // dereference and render an Object (the [^] operato
 struct Setter : Node {
     Object* object;
 
-    void render(int fd, Object* scope, bool dereference) {
-        scope -> replace(object); // doesn't write anything, just swaps in the new object
+    void render(int fd, Object* scope, bool dereference) { // doesn't write anything, just swaps in the new object
+        bool didReplace = false;
+        didReplace |= scope -> replace(object);
         if (parent != NULL) {
-            parent -> replace(object);
+            didReplace |= parent -> replace(object);
+        }
+        if (!didReplace) { // if we can't find the object we're supposed to replace, just shove it in the highest applicable point on the tree
+            parent -> addChild(object);
         }
     }
 
@@ -323,7 +398,6 @@ size_t fillObject(const char* from, size_t length, Object* container) { // desig
                 i ++;
             }
             char tagOp = from[tagStart];
-            printf("tagop is %c\n", tagOp);
             size_t tagDataSize = i - tagStart - 1; // the -1 is because we consumed the first byte, the operation code
             const char* tagData = from + tagStart + 1; // the +1 is for the same reason as above
             while (tagData[0] == ' ') {tagData ++;tagDataSize --;} // trim whitespace from the start
@@ -376,7 +450,7 @@ size_t fillObject(const char* from, size_t length, Object* container) { // desig
                 container -> addChild(i);
             }
             else {
-                printf("WARNING: Unrecognized tag operation %c! Parsing will continue, but the result may be malformed.\n", tagOp);
+                printf(WARNING "Unrecognized tag operation %c! Parsing will continue, but the result may be malformed.\n", tagOp);
             }
         }
         else if (from[i] == ']') {
@@ -404,6 +478,10 @@ Object string2object(const char* data, size_t length) {
     if (strncmp(data, "[!]", 3) == 0) { // it's a valid Sitix file
         fillObject(data + 3, length - 3, &ret);
     }
+    else if (strncmp(data, "[?]", 3) == 0) {
+        fillObject(data + 3, length - 3, &ret);
+        ret.isTemplate = true;
+    }
     else {
         PlainText* text = new PlainText;
         text -> data = data;
@@ -415,40 +493,95 @@ Object string2object(const char* data, size_t length) {
 
 
 void renderFile(const char* in, const char* out) {
-    printf("INFO: Rendering %s to %s.\n", in, out);
+    printf(INFO "Rendering %s to %s.\n", in, out);
     struct stat buf;
     stat(in, &buf);
     size_t size = buf.st_size;
-    int input = open(in, O_RDONLY);
-    if (input == -1) {
-        printf("ERROR: Couldn't open %s! This file will not be rendered.\n", in);
+    if (size == 0) {
+        printf(WARNING "%s has zero size! This file will not be rendered.\n\tIf you absolutely need an empty file there, consider writing a custom script that creates it after invoking Sitix.\n", in);
         return;
     }
-    int output = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666); // rw for anyone, it's not a protected file (this may lead to security vulnerabilities in the future)
-    // TODO: inherit permissions from the parent directory by some intelligent strategy so Sitix doesn't accidentally give attackers access to secure information
-    // contained in a statically generated website.
-    if (output == -1) {
-        close(input); // housekeeping
-        printf("ERROR: Couldn't open output file %s (for %s). This file will not be rendered.\n", out, in);
+    int input = open(in, O_RDONLY);
+    if (input == -1) {
+        printf(ERROR "Couldn't open %s! This file will not be rendered.\n", in);
         return;
     }
     char* inMap = (char*)mmap(0, size, PROT_READ, MAP_SHARED, input, 0); // it can handle obscenely large files because of map_shared
     if (inMap == MAP_FAILED) {
         close(input); // housekeeping
-        close(output);
-        printf("ERROR: Couldn't memory map %s! This file will not be rendered.\n", in);
+        printf(ERROR "Couldn't memory map %s! This file will not be rendered.\n", in);
         return;
     }
 
     Object file = string2object(inMap, size);
-    file.render(output, &file, true);
+    if (file.isTemplate) {
+        printf(INFO "%s is a template file, will not be rendered.\n", in);
+    }
+    else {
+        int output = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666); // rw for anyone, it's not a protected file (this may lead to security vulnerabilities in the future)
+        // TODO: inherit permissions from the parent directory by some intelligent strategy so Sitix doesn't accidentally give attackers access to secure information
+        // contained in a statically generated website.
+        if (output == -1) {
+            close(input); // housekeeping
+            printf(ERROR "Couldn't open output file %s (for %s). This file will not be rendered.\n", out, in);
+            return;
+        }
+        file.render(output, &file, true);
+        close(output);
+    }
     file.free();
     
-    close(output);
     close(input);
     munmap(inMap, size);
 }
 
+
+int iterRemove(const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) {
+    if (remove(fpath)) {
+        printf(WARNING "Encountered a non-fatal error while removing %s\n", fpath);
+    }
+    return 0;
+}
+
+void rmrf(const char* path) {
+    nftw(path, iterRemove, 64, FTW_DEPTH);
+}
+
+
+int renderRecursive(const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) {
+    char* thing = transmuted(siteDir, outputDir, fpath);
+    if (typeflag == FTW_D) {
+        mkdir(thing, 0700);
+    }
+    else {
+        renderFile(fpath, thing);
+    }
+    free(thing);
+    return FTW_CONTINUE;
+}
+
 int main(int argc, char** argv) {
-    renderFile("test/index.html", "testOut.html");
+    printf("\033[1mSitix v0.1 by Tyler Clarke\033[0m\n");
+    bool hasSpecificSitedir = false;
+    for (int i = 1; i < argc; i ++) {
+        if (strcmp(argv[i], "-o") == 0) {
+            i ++;
+            outputDir = argv[i];
+        }
+        else if (!hasSpecificSitedir) {
+            hasSpecificSitedir = true;
+            siteDir = argv[i];
+        }
+        else {
+            printf(ERROR "Unexpected argument %s\n", argv[i]);
+        }
+    }
+    printf(INFO "Cleaning output directory\n");
+    rmrf(outputDir);
+    mkdir(outputDir, 0700);
+    printf(INFO "Output directory clean.\n");
+    printf(INFO "Rendering project '%s' to '%s'.\n", siteDir, outputDir);
+    nftw(siteDir, renderRecursive, 64, 0);
+    printf("\033[1;33mComplete!\033[0m\n");
+    return 0;
 }
