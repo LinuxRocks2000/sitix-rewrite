ba48d2523b9cf06898cde976d4ad035882c23692
Lazy Hippopotamus <lazy@hippo.com>
2024-03-23 19:52:39 -0400

    add filenameify to evals

--- evals.hpp
+++ evals.hpp
@@ -376,6 +376,32 @@ struct EvalsSession {
                         break;
                     }
                 }
+                else if (symbol == "filenameify") {
+                    // converts a string to a valid filename
+                    EvalsObject* string = stack[stack.size() - 1];
+                    stack.pop_back();
+                    std::string s = string -> toString();
+                    delete string;
+                    for (size_t i = 0; i < s.size(); i ++) {
+                        if (s[i] >= 'A' && s[i] <= 'Z') {
+                            s[i] -= 'A';
+                            s[i] += 'a';
+                        }
+                        else if (s[i] < 'a' || s[i] > 'z') {
+                            if (s[i] != '.' && s[i] != '_' && (s[i] < '0' || s[i] > '9')) {
+                                s[i] = '-';
+                                if (i > 0 && s[i - 1] == '-') {
+                                    for (size_t j = i; j < s.size() - 1; j ++) {
+                                        s[j] = s[j + 1];
+                                    }
+                                    s.resize(s.size() - 1);
+                                    i --;
+                                }
+                            }
+                        }
+                    }
+                    stack.push_back(new StringObject(s));
+                }
                 else if (symbol == "true") {
                     stack.push_back(new BooleanObject(true));
                 }
