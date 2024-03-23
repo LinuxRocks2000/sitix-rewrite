34d54d067ddc8efbeb62416cbecf4b4a3d966d67
Lazy Hippopotamus <lazy@hippo.com>
2024-03-23 19:59:51 -0400

    whoops, forgot to add trim

--- evals.hpp
+++ evals.hpp
@@ -402,6 +402,24 @@ struct EvalsSession {
                     }
                     stack.push_back(new StringObject(s));
                 }
+                else if (symbol == "trim") {
+                    EvalsObject* string = stack[stack.size() - 1];
+                    stack.pop_back();
+                    std::string s = string -> toString();
+                    delete string;
+                    size_t trimStart, trimEnd;
+                    for (trimStart = 0; trimStart < s.size(); trimStart ++) {
+                        if (!isWhitespace(s[trimStart])) {
+                            break;
+                        }
+                    }
+                    for (trimEnd = s.size() - 1; trimEnd >= 0; trimEnd --) {
+                        if (!isWhitespace(s[trimEnd])) {
+                            break;
+                        }
+                    }
+                    stack.push_back(new StringObject(s.substr(trimStart, trimEnd - trimStart)));
+                }
                 else if (symbol == "true") {
                     stack.push_back(new BooleanObject(true));
                 }
